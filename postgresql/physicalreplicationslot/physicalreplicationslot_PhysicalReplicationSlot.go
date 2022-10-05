3571d3f42090b16ee8fe2f2ce4cb566c24a48f55
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-10-05 00:30:06 +0000

    chore(release): v3.0.2

--- postgresql/physicalreplicationslot/physicalreplicationslot_PhysicalReplicationSlot.go
+++ postgresql/physicalreplicationslot/physicalreplicationslot_PhysicalReplicationSlot.go
@@ -2,10 +2,10 @@ package physicalreplicationslot
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/hashicorp/cdktf-provider-postgresql-go/postgresql/v3/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v3/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/hashicorp/cdktf-provider-postgresql-go/postgresql/v3/physicalreplicationslot/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v3/physicalreplicationslot/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
