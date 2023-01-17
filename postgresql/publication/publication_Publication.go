171c30b17df346425f7cb3f981c89b5ce97d3c1a
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:47:08 +0000

    chore(release): v5.0.0

--- postgresql/publication/publication_Publication.go
+++ postgresql/publication/publication_Publication.go
@@ -2,10 +2,10 @@ package publication
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v5/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v4/publication/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v5/publication/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
