1fea854e0245f61d526cae53262cf27bf3d5a045
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-06-15 11:33:17 +0000

    chore(release): v7.0.0

--- postgresql/datapostgresqlsequences/DataPostgresqlSequencesSequencesList.go
+++ postgresql/datapostgresqlsequences/DataPostgresqlSequencesSequencesList.go
@@ -2,9 +2,9 @@ package datapostgresqlsequences
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v6/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v7/jsii"
 
-	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v6/datapostgresqlsequences/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v7/datapostgresqlsequences/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
