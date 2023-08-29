3867775e720c0ccd227503ec1c9d417e09c85734
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-08-29 15:00:49 +0000

    chore(release): v9.0.0

--- postgresql/functionresource/FunctionResource.go
+++ postgresql/functionresource/FunctionResource.go
@@ -1,11 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package functionresource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v8/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v9/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v8/functionresource/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v9/functionresource/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- postgresql/server/Server.go
+++ postgresql/server/Server.go
@@ -1,11 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package server
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v8/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v9/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v8/server/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v9/server/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
