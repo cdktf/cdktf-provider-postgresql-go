63c7cea4e16cf466d0d89fbf4fee7a2490cf1540
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-06-04 19:01:31 +0000

    chore(release): v12.0.0

--- postgresql/datapostgresqlsequences/DataPostgresqlSequencesSequencesOutputReference.go
+++ postgresql/datapostgresqlsequences/DataPostgresqlSequencesSequencesOutputReference.go
@@ -5,9 +5,9 @@ package datapostgresqlsequences
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v11/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v12/jsii"
 
-	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v11/datapostgresqlsequences/internal"
+	"github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v12/datapostgresqlsequences/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- postgresql/go.mod
+++ postgresql/go.mod
@@ -1,9 +1,9 @@
-module github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v11
+module github.com/cdktf/cdktf-provider-postgresql-go/postgresql/v12
 
 go 1.18
 
 require (
-	github.com/aws/jsii-runtime-go v1.105.0
-	github.com/hashicorp/terraform-cdk-go/cdktf v0.20.10
+	github.com/aws/jsii-runtime-go v1.111.0
+	github.com/hashicorp/terraform-cdk-go/cdktf v0.21.0
 	github.com/aws/constructs-go/constructs/v10 v10.4.2
 )
